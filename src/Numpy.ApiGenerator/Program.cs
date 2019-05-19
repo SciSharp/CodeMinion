eb83e7be80ac62dbe8515beca7298d0987521c5f
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-05-19 16:00:39 +0200

    added Torch and Numpy API generators

---
+++ src/Numpy.ApiGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Torch.ApiGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numpy_gen = new NumpyApiGenerator();
+            numpy_gen.Generate();
+        }
+    }
+}
---
+++ src/Torch.ApiGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Torch.ApiGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var torch_gen = new TorchApiGenerator();
+            torch_gen.Generate();
+        }
+    }
+}
