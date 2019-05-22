9c5f330fc684f5029f8cd62a07b4b8eff1982af3
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-05-22 11:50:00 +0200

    added method generation for API objects

--- src/Numpy/Models/NDarray.cs
+++ src/Numpy/Models/NDarray.cs
@@ -66,42 +66,42 @@ namespace Numpy
         /// <summary>
         /// Information about the memory layout of the array.
         /// </summary>
-        public Flags flags => new Flags(_pobj.GetAttr("flags")); // TODO: implement Flags
+        public Flags flags => new Flags(self.GetAttr("flags")); // TODO: implement Flags
 
         /// <summary>
         /// Tuple of array dimensions.
         /// </summary>
-        public Shape shape => _pobj.GetAttr("shape").As<int[]>();
+        public Shape shape => self.GetAttr("shape").As<int[]>();
 
         /// <summary>
         /// Tuple of bytes to step in each dimension when traversing an array.
         /// </summary>
-        public int[] strides => _pobj.GetAttr("strides").As<int[]>();
+        public int[] strides => self.GetAttr("strides").As<int[]>();
 
         /// <summary>
         /// Number of array dimensions.
         /// </summary>
-        public int ndim => _pobj.GetAttr("ndim").As<int>();
+        public int ndim => self.GetAttr("ndim").As<int>();
 
         /// <summary>
         /// Python buffer object pointing to the start of the array’s data.
         /// </summary>
-        public PyObject data => _pobj.GetAttr("data");
+        public PyObject data => self.GetAttr("data");
 
         /// <summary>
         /// Number of elements in the array.
         /// </summary>
-        public int size => _pobj.GetAttr("size").As<int>();
+        public int size => self.GetAttr("size").As<int>();
 
         /// <summary>
         /// Length of one array element in bytes.
         /// </summary>
-        public int itemsize => _pobj.GetAttr("itemsize").As<int>();
+        public int itemsize => self.GetAttr("itemsize").As<int>();
 
         /// <summary>
         /// Total bytes consumed by the elements of the array.
         /// </summary>
-        public int nbytes => _pobj.GetAttr("nbytes").As<int>();
+        public int nbytes => self.GetAttr("nbytes").As<int>();
 
         /// <summary>
         /// Base object if memory is from some other object.
@@ -110,7 +110,7 @@ namespace Numpy
         {
             get
             {
-                PyObject base_obj = _pobj.GetAttr("base");
+                PyObject base_obj = self.GetAttr("base");
                 if (base_obj.IsNone())
                     return null;
                 return new NDarray(base_obj);
@@ -120,38 +120,38 @@ namespace Numpy
         /// <summary>
         /// Data-type of the array’s elements.
         /// </summary>
-        public Dtype dtype => new Dtype(_pobj.GetAttr("dtype"));
+        public Dtype dtype => new Dtype(self.GetAttr("dtype"));
 
         /// <summary>
         /// Same as self.transpose(), except that self is returned if self.ndim &lt; 2.
         /// </summary>
-        public NDarray T => new NDarray(_pobj.GetAttr("T"));
+        public NDarray T => new NDarray(self.GetAttr("T"));
 
         /// <summary>
         /// The real part of the array.
         /// </summary>
-        public NDarray real => new NDarray(_pobj.GetAttr("real"));
+        public NDarray real => new NDarray(self.GetAttr("real"));
 
         /// <summary>
         /// The imaginary part of the array.
         /// </summary>
-        public NDarray imag => new NDarray(_pobj.GetAttr("imag"));
+        public NDarray imag => new NDarray(self.GetAttr("imag"));
 
         /// <summary>
         /// A 1-D iterator over the array.
         /// </summary>
-        public PyObject flat => _pobj.GetAttr("flat"); // todo: wrap and support usecases
+        public PyObject flat => self.GetAttr("flat"); // todo: wrap and support usecases
 
         /// <summary>
         /// An object to simplify the interaction of the array with the ctypes module.
         /// </summary>
-        public PyObject ctypes => _pobj.GetAttr("ctypes"); // TODO: wrap ctypes
+        public PyObject ctypes => self.GetAttr("ctypes"); // TODO: wrap ctypes
 
 
         /// <summary>
         /// Length of the array (same as size)
         /// </summary>
-        public int len => _pobj.InvokeMethod("__len__").As<int>();
+        public int len => self.InvokeMethod("__len__").As<int>();
     }
 
     public class NDarray<T> : NDarray
