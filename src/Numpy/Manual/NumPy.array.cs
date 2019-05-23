5e829461562092abc9bbda767cd0a9f1a61ae034
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-05-23 21:47:11 +0200

    np.array overload

--- src/Numpy/Manual/NumPy.array.cs
+++ src/Numpy/Manual/NumPy.array.cs
@@ -87,4 +87,22 @@ namespace Numpy
         }
 
     }
+
+    public static partial class np
+    {
+
+        /// <summary>
+        /// Create an array.
+        /// 
+        /// <param name="data">
+        /// The array to initialize the ndarray with
+        /// </param>
+        /// <returns>
+        /// An array object satisfying the specified requirements.
+        /// </returns>
+        public static NDarray<T> array<T>(params T[] data)
+        {
+            return NumPy.Instance.array(data);
+        }
+    }
 }
