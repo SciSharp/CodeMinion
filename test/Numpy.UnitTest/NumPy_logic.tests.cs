9843f3e623ca707d3eb6cf3400d67ccefb26ef0b
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-06-01 18:07:27 +0200

    Shape: add indexer for dimensions

--- test/Numpy.UnitTest/NumPy_logic.tests.cs
+++ test/Numpy.UnitTest/NumPy_logic.tests.cs
@@ -8,8 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using Python.Runtime;
-using Python.Included;
 using Numpy.Models;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
